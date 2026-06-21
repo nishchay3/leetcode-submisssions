6a99531df5b96baa8e3ca412f45a789c50169418
Nishchay Maurya <62929166+nishchay3@users.noreply.github.com>
2026-06-21 20:11:16 +0530

    Time: 33 ms (32.71%) | Memory: 59.6 MB (29.32%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+    let resultLength = 1;
+    let result = "";
+    if (s.length < 2) return s;
+
+    for (i = 0; i < s.length; i++) {
+        let left = i - 1;
+        let right = i + 1;
+        // Odd
+        while (i > 0 && left >= 0 && right <= s.length - 1 && s[left] === s[right]) {
+            if (right - left + 1 > resultLength) {
+                result = s.slice(left, right + 1);
+                resultLength = right - left + 1;
+            }
+            left--;
+            right++;
+        }
+
+        left = i;
+        right = i + 1;
+        // Even
+        while (left >= 0 && right <= s.length - 1 && s[left] === s[right]) {
+            if (right - left + 1 > resultLength) {
+                result = s.slice(left, right + 1);
+                resultLength = right - left + 1;
+            }
+            left--;
+            right++;
+        }
+    }
+
+    if (!result.length) return s[0];
+    return result;
+};
